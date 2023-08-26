c6e87a0ef4d2ba6a8a6bc73d6e35d20bcf39d488
Richard Zheng <ruixiangzheng95@gmail.com>
2023-08-26 18:01:43 -0500

    Initial commit

---
+++ src/main/java/com/rzheng/jenkinsdemo/Controller.java
@@ -0,0 +1,13 @@
+package com.rzheng.jenkinsdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/test")
+    public String hello() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/rzheng/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rzheng.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
