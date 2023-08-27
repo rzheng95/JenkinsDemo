482db0cebd06023447fee6bb8770f1e6ab3db11c
Richard Zheng <ruixiangzheng95@gmail.com>
2023-08-26 19:33:38 -0500

    Add print statement

--- src/main/java/com/rzheng/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/com/rzheng/jenkinsdemo/JenkinsDemoApplication.java
@@ -8,6 +8,7 @@ public class JenkinsDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsDemoApplication.class, args);
+        System.out.println("Hello World! Richard");
     }
 
 }
